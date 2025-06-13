176d658c8cfad42a992136c92c7852d908478ab5
Anas-tec <a.anas.muhammed25@gmail.com>
2025-06-13 20:18:50 +0530

    <>|Multiple Interface & Repository|<Anas>

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,59 +1,38 @@
 ﻿using Api_Test.Models.Data;
 using Api_Test.Models.Entities;
 using Api_Test.Models.StudentsDto;
+using Api_Test.Repository;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api_Test.Services
 {
-    public class StudentService : IStudent,IStudentUD
+    public class StudentService : IStudent, IStudentUD
     {
-        private readonly ApplicationDbContext _dbContext;
-        public StudentService(ApplicationDbContext dbContext)
+        private readonly StudentRepository _repository;
+        public StudentService(StudentRepository repository)
         {
-            _dbContext = dbContext;
+            _repository = repository;
         }
-
         public StudentService()
         {
         }
-
         public List<Student> GetStudents()
         {
-            return _dbContext.Students.ToList();
+            return _repository.GetStudents();
         }
         public void AddStudents(AddStudentDto addStudentDto)
         {
-            var stu = new Student()
-            {
-                Name = addStudentDto.Name,
-                Age = addStudentDto.Age,
-                FatherName = addStudentDto.FatherName
-            };
-            _dbContext.Students.Add(stu);
-            _dbContext.SaveChanges();
+            _repository.AddStudents(addStudentDto);
         }
         public bool UpdateStudent(int id, UpdateStudent dto)
         {
-            var student = _dbContext.Students.Find(id);
-            if (student == null) return false;
-
-            student.Name = dto.Name;
-            student.Age = dto.Age;
-            student.FatherName = dto.FatherName;
-
-            _dbContext.SaveChanges();
-            return true;
+            return _repository.UpdateStudent(id, dto);
         }
-
         public bool DeleteStudent(int id)
         {
-            var student = _dbContext.Students.Find(id);
-            if (student == null) return false;
-
-            _dbContext.Students.Remove(student);
-            _dbContext.SaveChanges();
-            return true;
+            return _repository.DeleteStudent(id);
         }
     }
 }
